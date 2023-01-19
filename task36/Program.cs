4eeb4a1d5a83ca9716cf77f0ed4d2b91b3beb44f
Sergey <aseev-1977@list.ru>
2023-01-19 20:56:29 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
+int [] numbers = new int[size];
+Console.ResetColor ();
+
+FillArrayRandomNumbers(numbers);
+
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        result += numbers[i];
+    }
+}
+
+void FillArrayRandomNumbers(int [] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+int ReadInt(string message)
+
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.Write("[");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+         if(numbers[i] != ',')
+         {
+                Console.Write(numbers[i]);
+                if(i == numbers.Length -1)
+                   Console.Write("");
+                else
+                 Console.Write(",");
+         }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+    Console.ResetColor ();
+}
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
+Console.ResetColor ();
